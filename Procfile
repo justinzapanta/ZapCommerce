0a678ca10558f939bb7f8a5a082b9805343a5d96
Justin Zapanta <zapantajustin5@gmail.com>
2024-11-20 04:30:30 +0800

    deploy

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn Zapcommerce.wsgi
--- Zapcommerce/settings.py
+++ Zapcommerce/settings.py
@@ -22,11 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-%_e+_1ts!q$=$9!q=$mmmhhscjkvz@--8md)%_fh@#ky9m8z_+'
+SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-%_e+_1ts!q$=$9!q=$mmmhhscjkvz@--8md)%_fh@#ky9m8z_+')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '192.168.0.114']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
@@ -118,6 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 MEDIA_URL = 'media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
