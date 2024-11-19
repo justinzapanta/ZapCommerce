f605c3c8f7a08b040900ddbe1e040442cac5ec95
Justin Zapanta <zapantajustin5@gmail.com>
2024-11-20 04:51:54 +0800

    Working on stripe

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: gunicorn Zapcommerce.wsgi
--- Zapcommerce/settings.py
+++ Zapcommerce/settings.py
@@ -22,12 +22,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-%_e+_1ts!q$=$9!q=$mmmhhscjkvz@--8md)%_fh@#ky9m8z_+'
-SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-%_e+_1ts!q$=$9!q=$mmmhhscjkvz@--8md)%_fh@#ky9m8z_+')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1',  '192.168.0.114', '.vercel.app' ]
 
 
 # Application definition
@@ -119,7 +118,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
 MEDIA_URL = 'media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
